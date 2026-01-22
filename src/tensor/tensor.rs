83196555878345b7926cbe3d9a1d34dd6118d58e
Priyanshu Soni <priyanshu171203@gmail.com>
2026-01-22 12:45:01 +0530

    unit tests for tensor

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -11,17 +11,39 @@ impl Tensor {
         let data_len = data.len();
         let expected = metadata.total_byte_size()?;
         if data_len != expected {
-            return Err("Data length does not match expected length (calculated from tensor metadata");
+            return Err("Data length does not match expected length (calculated from tensor metadata)");
         }
         Ok(Self { metadata, data })
     }
 }
 
+#[cfg(test)]
 mod tests{
     use super::*;
+    use crate::tensor::meta::{DType, StorageLayout, TensorMeta};
+    #[test]
+    fn test_tensor_new_valid() {
+        let meta = TensorMeta::new(
+            DType::F32,
+            vec![4,4],
+            StorageLayout::RowMajor
+        ).unwrap();
+        //the meta data is 64 bytes in size, this one should be too.
+        let data = vec![0u8; 64];
+        let tensor = Tensor::new(meta, data);
+        assert!(tensor.is_ok());
+    }
 
     #[test]
-    fn test_tensor_new() {
-        
+    fn test_tensor_new_invalid() {
+        let meta = TensorMeta::new(
+            DType::F32,
+            vec![4,4],
+            StorageLayout::RowMajor
+        ).unwrap();
+
+        let data = vec![0u8; 63];
+        let tensor = Tensor::new(meta, data);
+        assert!(tensor.is_err());
     }
 }

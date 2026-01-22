dd0b334ac67c8df14a11ff977a51d036e0978761
Priyanshu Soni <priyanshu171203@gmail.com>
2026-01-22 13:05:51 +0530

    core caching logic and guarantees.md

---
+++ src/cache/cache.rs
@@ -0,0 +1,8 @@
+//actual caching logic
+//the put()/get() request arrives here via the exposed API in lib.rs , we use a in memory hashmap
+// to store data in KV pairs.
+// hashing strategy-> ?
+// eviction strategy -> ?
+pub struct Cache {
+    
+}

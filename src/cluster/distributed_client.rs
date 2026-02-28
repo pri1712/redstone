78c4e72cfe2d80320a470d1d8571667a3f5027d2
Priyanshu Soni <priyanshu171203@gmail.com>
2026-02-28 07:03:44 +0530

    client side routing logic

--- src/cluster/distributed_client.rs
+++ src/cluster/distributed_client.rs
@@ -2,7 +2,7 @@ use std::iter::Map;
 use std::sync::{Arc};
 use parking_lot::RwLock;
 use std::collections::HashMap;
-
+use crate::cluster::client_config::ClusterClientConfig;
 use crate::transport::grpc::client::RemoteCacheClient;
 use crate::cluster::ring::HashRing;
 /// Cluster-aware client that routes cache operations to the correct
@@ -17,11 +17,11 @@ pub struct DistributedClient {
     //map servers node name to a single remoteCacheClient instance,
     clients: Arc<RwLock<HashMap<String, RemoteCacheClient>>>,
     ring: Arc<RwLock<HashRing>>,
-
+    client_config: ClusterClientConfig
 }
 
 impl DistributedClient {
-    fn new(clients: Arc<RwLock<HashMap<String, RemoteCacheClient>>>, ring: Arc<RwLock<HashRing>>) -> Self {
-        Self { clients, ring }
+    fn new(client_config: ClusterClientConfig) -> DistributedClient {
+
     }
 }
--- src/cluster/mod.rs
+++ src/cluster/mod.rs
@@ -2,3 +2,4 @@ pub mod distributed_client;
 mod hashing;
 mod node;
 mod ring;
+mod client_config;
